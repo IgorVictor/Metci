79e7746fc9cb9e971a38e57c5c9366a884e35e0f
IgorVictor <magusdovento@gmail.com>
2017-03-07 11:29:19 -0300

    changing the HomogeneousController to be modulable

--- src/main/java/ProjetoMetci/metci/HomogeneousController.java
+++ src/main/java/ProjetoMetci/metci/HomogeneousController.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public class HomogeneousController {
 
-	public void start(String filePath) {
+	public void start(String filePath, int serverCount) {
 		ArrayList<ComputerServer> serverList = new ArrayList<ComputerServer>();
 		
-		for (int i = 0; i < 16; i++) {
-			ComputerServer server = new ComputerServer(Integer.toString(i), 446.875, 1024.0);
+		for (int i = serverCount; i < 16; i++) {
+			ComputerServer server = new ComputerServer(Integer.toString(i), 7150.0/serverCount, 1024.0);
 			
 			serverList.add(server);
 		}
