8e4a021cf2c293047685f352ea6b3136b7d98a9f
Dennis Dantas <ddspog@gmail.com>
2017-03-08 16:43:03 -0300

    Add equals and hashcode to NodePower :art:
    I've added equals and hashcode to NodePower for comparing reasons.

--- src/main/java/ProjetoMetci/metci/NodePower.java
+++ src/main/java/ProjetoMetci/metci/NodePower.java
@@ -67,4 +67,26 @@ public class NodePower {
 		return this;
 	}
 
+	public boolean equals(Object object) {
+		if (this == object) return true;
+		if (object == null || getClass() != object.getClass()) return false;
+		if (!super.equals(object)) return false;
+
+		NodePower nodePower = (NodePower) object;
+
+		if (Double.compare(nodePower.processor, processor) != 0) return false;
+		if (Double.compare(nodePower.ram, ram) != 0) return false;
+
+		return true;
+	}
+
+	public int hashCode() {
+		int result = super.hashCode();
+		long temp;
+		temp = Double.doubleToLongBits(processor);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(ram);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 }
