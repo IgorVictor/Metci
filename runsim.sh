ef51d8c43b5354c156aecb68ff28c8574e611887
IgorVictor <magusdovento@gmail.com>
2017-03-16 23:24:54 -0300

    Added bash script

---
+++ runsim.sh
@@ -0,0 +1,4 @@
+java TheJavaFile "$@"
+
+--first argument is the filepath of the trace second argument is the number of servers (usually 13000), third argument is is the servers should be
+--homogeneous fourth param is the algorithm, 1 for Best fit, 2 for worst fit, anything else for First fit, fifth param is if the servers have or not a possbility of failure
